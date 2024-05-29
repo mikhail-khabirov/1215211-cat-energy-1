38502ce6f00c5404d6bac234c8d8859f2fcb727d
Mikhail Khabirov <kyle@bk.ru>
2024-05-29 20:08:28 +0500

    feat: additional optimization

--- source/js/burger.js
+++ source/js/burger.js
@@ -1,9 +1,13 @@
 const siteNavigation = document.querySelector('.site-navigation');
 const navButton = document.querySelector('.site-navigation__button');
 
+
+// REMOVES CLASS "NO-JS"
 siteNavigation.classList.remove('site-navigation--no-js');
 
+// BURGER MENU
 navButton.addEventListener('click', function () {
   navButton.classList.toggle('site-navigation__button--is-pressed');
   siteNavigation.classList.toggle('site-navigation--is-opened');
 })
+
---
+++ source/js/slider.js
@@ -0,0 +1,49 @@
+const toggleButton = document.querySelector('.example__toggle-button');
+const range = document.querySelector('.example__picture-wrapper');
+const leftImage = document.querySelector('.example__image--before');
+const rightImage = document.querySelector('.example__image--after');
+
+toggleButton.onmousedown = function (e) {
+  e.preventDefault();
+  let shiftX = e.clientX - toggleButton.getBoundingClientRect().left;
+
+  document.addEventListener('mousemove', onMouseMove);
+  document.addEventListener('mouseup', onMouseUp);
+
+  
+  function onMouseMove(e) {
+    let newLeft = e.clientX - shiftX - range.getBoundingClientRect().left;
+    let newRight = e.clientX - shiftX - range.getBoundingClientRect().right;
+
+    if (newLeft < 0) {
+      newLeft = 0;
+    }
+
+    if (newRight > 0) {
+      newRight = 0;
+    }
+
+    let rightEdge = range.offsetWidth - toggleButton.offsetWidth;
+    let leftEdge = toggleButton.offsetWidth - range.offsetWidth;
+
+    if (newLeft > rightEdge) {
+      newLeft = rightEdge;
+    }
+
+    if (newRight < leftEdge) {
+      newRight = leftEdge;
+    }
+
+    toggleButton.style.left = newLeft + 'px';
+    toggleButton.style.right = newRight + 'px';
+
+    leftImage.style.width = newLeft  + 'px';
+    rightImage.style.width = -newRight + 'px';
+
+  }
+
+  function onMouseUp() {
+    document.removeEventListener('moseup', onMouseUp);
+    document.removeEventListener('mousemove', onMouseMove);
+  }
+}
